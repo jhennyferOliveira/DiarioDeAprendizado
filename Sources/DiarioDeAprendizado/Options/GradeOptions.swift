53fd5f6b243a8986197651fdbcc93c5af7c50297
jhennyferOliveira <jhennyfer519@gmail.com>
2020-03-30 10:09:39 -0300

    transfer function related to grade to gradeOptions

--- Sources/DiarioDeAprendizado/Options/GradeOptions.swift
+++ Sources/DiarioDeAprendizado/Options/GradeOptions.swift
@@ -9,20 +9,15 @@ import Foundation
 
 protocol GradeOptionsDelegate: class {
     func createNewGrade()
-    func searchGradeByName(nome : String)
+    func searchGradeByName(nome : String) -> Subject?
     func showGrades()
 }
-
 
 public class GradeOptions: GradeOptionsDelegate {
     
     let folderPath = FileManager.default.currentDirectoryPath + "/json"
     let completePathSubject = FileManager.default.currentDirectoryPath + "/json/disciplina.txt"
     // let completePathAluno = FileManager.default.currentDirectoryPath + "/json/aluno.txt"
-    
-    func searchGradeByName(nome: String) {
-        
-    }
     
     func createNewGrade() {
         let service = Service<Subject>()
@@ -51,6 +46,17 @@ public class GradeOptions: GradeOptionsDelegate {
         print("sua disciplina foi salva no arquivo disciplina.txt")
     }
     
+    func searchGradeByName(nome: String) -> Subject?{
+        let service_subject = Service<Subject>()
+        let arrayDisciplinas : [Subject] = service_subject.read(filePath: completePathSubject)
+        for disciplina in arrayDisciplinas {
+            if(disciplina.nome == nome) {
+                return disciplina
+            }
+        }
+        return nil
+    }
+    
     func showGrades() {
         let service = Service<Subject>()
         let grades = service.read(filePath: completePathSubject)
@@ -59,12 +65,12 @@ public class GradeOptions: GradeOptionsDelegate {
         }.reduce("") {$0 + "\n" + $1}
         
         print("""
-        Disciplinas Cadastradas:
-        \(subjects)
-
-        """)
-        
+            Disciplinas Cadastradas:
+            \(subjects)
+            
+            """)
     }
+    
     func autoIncrementSubjectId() -> Int{
         let service = Service<Subject>()
         let arraySubject : [Subject] = service.read(filePath: completePathSubject)
