e42b0e61bac6f28c0cf488db0971f2c5e181f6df
jhennyferOliveira <jhennyfer519@gmail.com>
2020-03-31 10:45:00 -0300

    add functions select subject and edit subject

--- Sources/DiarioDeAprendizado/Options/SubjectOptions.swift
+++ Sources/DiarioDeAprendizado/Options/SubjectOptions.swift
@@ -11,13 +11,20 @@ enum SubjectSearchBy {
     case name, id
 }
 
+enum Edit{
+    case name
+    case grade1
+    case grade2
+}
+
 protocol SubjectOptionsDelegate: class {
     func create(name: String, n1: String, n2: String, links: String?)
     func search(parameter: String, search: SubjectSearchBy) -> Subject?
     func details()
     func delete()
-    func edit()
+    func edit(subject: Subject, edit: Edit, newValue: String)
     func average(subject: Subject, weightN1: Int, weightN2: Int) throws -> Double
+    func selectSubjectById() -> Subject?
 }
 
 public class SubjectOptions: SubjectOptionsDelegate {
@@ -91,8 +98,8 @@ public class SubjectOptions: SubjectOptionsDelegate {
     
     func details(){
         let grades = service.read(filePath: completePathSubject)
-        let subjects = grades.enumerated().map { (index, subject) in
-            return "\(index) - \(subject.nome)"
+        let subjects = grades.map { subject in
+            return "\(subject.id) - \(subject.nome)"
         }.reduce("") {$0 + "\n" + $1}
         
         if !subjects.isEmpty {
@@ -110,10 +117,44 @@ public class SubjectOptions: SubjectOptionsDelegate {
         
     }
     
-    func edit() {
+    
+    func edit(subject: Subject, edit: Edit, newValue: String) {
+        var newSubject = Subject()
+        newSubject = subject
         
+        switch edit {
+        case .name:
+            newSubject.nome = newValue
+        case .grade1:
+            newSubject.nota1 = newValue
+        case .grade2:
+            newSubject.nota2 = newValue
+        }
+        service.deleteById(filePath: completePathSubject, id: subject.id)
+        service.save(object: newSubject, folderPath: completePathSubject)
     }
     
+    
+    func selectSubjectById() -> Subject? {
+        
+        let subjects : [Subject] = service.read(filePath: completePathSubject)
+        if(!subjects.isEmpty){
+            print("Digite o id para selecionar a disciplina: ")
+            if let input = readLine() {
+                if let input = Int(input) {
+                    for subject in subjects{
+                        if (subject.id == input){
+                            return subject
+                        }
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        return nil
+    }
     
     // error propagation
     func average(subject: Subject, weightN1: Int, weightN2: Int) throws -> Double  {
