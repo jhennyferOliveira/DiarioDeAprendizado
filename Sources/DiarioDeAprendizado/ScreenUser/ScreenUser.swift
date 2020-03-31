dfc2e974c02ed2fd3b878ea85be8c6bd6f6fd4d7
vinicius <viniciuscoelho.ti@gmail.com>
2020-03-31 14:24:13 -0300

    update cases

--- Sources/DiarioDeAprendizado/ScreenUser/ScreenUser.swift
+++ Sources/DiarioDeAprendizado/ScreenUser/ScreenUser.swift
@@ -30,7 +30,8 @@ public class ScreenUser {
             delegate?.editInformation()
         case .saveInformation:
             register()
-        
+        case .login:
+            login()
         }
     }
     
